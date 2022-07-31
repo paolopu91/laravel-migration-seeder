4cd1b51d46ca83ebac66f56411682b845ba58556
paolopu91 <paolo.pugliese1991@gmail.com>
2022-07-31 19:12:46 +0200

    added page title and changed file.env

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+
+@section('page_title','Trains')
+
+@section('page_content',)
+    
+
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,5 @@
+<div class="section-header">
+    <div class="container text-center">
+        <h1 class="text-primary">Lista dei miei treni</h1>
+    </div>
+</div>
